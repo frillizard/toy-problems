0f13e8573d19dcecf32f9dcd81df6e1fc1103fa2
Rikki Sasda <rikkisasda@gmail.com>
2019-01-27 21:51:18 -0600

    added binaryToDecimal.js

---
+++ binaryToDecimal.js
@@ -0,0 +1,12 @@
+const binaryToDecimal = (n) => {
+  let decimal = 0;
+  let binary = [...n].reverse();
+  for (let i = 0; i < n.length; i++) {
+    if (binary[i] > 0) {
+      decimal += Math.pow(2, i);
+    }
+  }
+  return decimal;
+}
+
+// Note: Do NOT use partInt(n, 2)
